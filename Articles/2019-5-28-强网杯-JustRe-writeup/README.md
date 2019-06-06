619a079bf53d5b8b6fefb004c4ffd2b0432ef8ba
dajun <2809704352@qq.com>
2019-06-06 19:20:07 +0800

    change the path

---
+++ Articles/2019-5-28-强网杯-JustRe-writeup/README.md
@@ -0,0 +1,3 @@
+# JustRe
+
+## WriteUp：[传送门](https://blog.csdn.net/weixin_42151611/article/details/90671904)
---
+++ Articles/2019-5-30-攻防世界-The Maya Society-writeup/Src/shell.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+CURR_TIME=1325347200
+LAST_TIME=1356969600
+hour=3600
+export CURR_TIME
+while [ $CURR_TIME -lt $LAST_TIME ]
+do
+    value=$(CURR_TIME=$CURR_TIME LD_PRELOAD=$(pwd)/faketime.so ./launcher)
+    if [ "$value" != "" ]
+    then
+        echo $value
+        exit 1
+    else
+        CURR_TIME=$[CURR_TIME+hour]
+    fi
+done
